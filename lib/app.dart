338cf170c4bffa513dc09e56fde32537ca6c573a
RainVisitor <abc873693@gmail.com>
2020-04-08 01:51:46 +0800

    Add feature login page

---
+++ lib/app.dart
@@ -0,0 +1,100 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:ap/pages/login_page.dart';
+import 'package:ap/widgets/share_data_widget.dart';
+import 'package:ap_common/models/ap_support_language.dart';
+import 'package:ap_common/resources/ap_theme.dart';
+import 'package:ap_common/utils/ap_localizations.dart';
+import 'package:ap_common/utils/preferences.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'config/constants.dart';
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  Brightness brightness = Brightness.light;
+  String username;
+  String password;
+
+  ThemeMode themeMode = ThemeMode.system;
+
+  @override
+  void initState() {
+    themeMode = ThemeMode
+        .values[Preferences.getInt(Constants.PREF_THEME_MODE_INDEX, 0)];
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    setState(() {});
+    super.didChangePlatformBrightness();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ShareDataWidget(
+      this,
+      child: ApTheme(
+        themeMode,
+        child: MaterialApp(
+//          onGenerateTitle: (context) => AppLocalizations.of(context).appName,
+          debugShowCheckedModeBanner: false,
+          routes: <String, WidgetBuilder>{
+            Navigator.defaultRouteName: (context) => LoginPage(),
+            LoginPage.routerName: (context) => LoginPage(),
+          },
+          theme: ApTheme.light,
+          darkTheme: ApTheme.dark,
+          themeMode: themeMode,
+//          navigatorObservers: !kIsWeb && (Platform.isAndroid || Platform.isIOS)
+//              ? [
+//                  FirebaseAnalyticsObserver(analytics: _analytics),
+//                ]
+//              : [],
+          localeResolutionCallback:
+              (Locale locale, Iterable<Locale> supportedLocales) {
+//            print('Load ${locale.languageCode}');
+            String languageCode = Preferences.getString(
+              Constants.PREF_LANGUAGE_CODE,
+              ApSupportLanguageConstants.SYSTEM,
+            );
+            if (languageCode == ApSupportLanguageConstants.SYSTEM)
+              return locale;
+            else
+              return Locale(languageCode);
+          },
+          localizationsDelegates: [
+            const ApLocalizationsDelegate(),
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+          ],
+          supportedLocales: [
+            const Locale('en', 'US'), // English
+            const Locale('zh', 'TW'), // Chinese
+          ],
+        ),
+      ),
+    );
+  }
+
+  void update(ThemeMode mode) {
+    setState(() {
+      themeMode = mode;
+    });
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,337 @@
+import 'dart:typed_data';
+
+import 'package:ap/api/course_helper.dart';
+import 'package:ap/config/constants.dart';
+import 'package:ap/utils/ocr_utils.dart';
+import 'package:ap_common/callback/general_callback.dart';
+import 'package:ap_common/models/general_response.dart';
+import 'package:ap_common/resources/ap_theme.dart';
+import 'package:ap_common/utils/ap_localizations.dart';
+import 'package:ap_common/utils/ap_utils.dart';
+import 'package:ap_common/utils/preferences.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ap_common/widgets/progress_dialog.dart';
+
+class LoginPage extends StatefulWidget {
+  static const String routerName = "/login";
+
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  ApLocalizations app;
+
+  final TextEditingController _username = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  final TextEditingController _validationCode = TextEditingController();
+
+  var isRememberPassword = true;
+  var isAutoLogin = false;
+
+  FocusNode usernameFocusNode = FocusNode();
+  FocusNode passwordFocusNode = FocusNode();
+  FocusNode validationCodeFocusNode = FocusNode();
+
+  Uint8List bodyBytes;
+
+  @override
+  void initState() {
+    super.initState();
+    _getPreference();
+    _getValidationCode();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  _editTextStyle() => TextStyle(
+      color: Colors.white, fontSize: 18.0, decorationColor: Colors.white);
+
+  @override
+  Widget build(BuildContext context) {
+    app = ApLocalizations.of(context);
+    return OrientationBuilder(
+      builder: (_, orientation) {
+        return Scaffold(
+          backgroundColor: ApTheme.of(context).blue,
+          resizeToAvoidBottomPadding: orientation == Orientation.portrait,
+          body: Container(
+            alignment: Alignment(0, 0),
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            child: orientation == Orientation.portrait
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    mainAxisSize: MainAxisSize.min,
+                    children: _renderContent(orientation),
+                  )
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: _renderContent(orientation),
+                  ),
+          ),
+        );
+      },
+    );
+  }
+
+  _renderContent(Orientation orientation) {
+    List<Widget> list = orientation == Orientation.portrait
+        ? <Widget>[
+            Center(
+              child: Text(
+                'T',
+                style: TextStyle(
+                  fontSize: 120,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            SizedBox(height: orientation == Orientation.portrait ? 30.0 : 0.0),
+          ]
+        : <Widget>[
+            Expanded(
+              child: Text(
+                'T',
+                style: TextStyle(
+                  fontSize: 120,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: orientation == Orientation.portrait ? 30.0 : 0.0),
+          ];
+    List<Widget> listB = <Widget>[
+      TextField(
+        maxLines: 1,
+        controller: _username,
+        textInputAction: TextInputAction.next,
+        focusNode: usernameFocusNode,
+        onSubmitted: (text) {
+          usernameFocusNode.unfocus();
+          FocusScope.of(context).requestFocus(passwordFocusNode);
+        },
+        decoration: InputDecoration(
+          labelText: app.username,
+        ),
+        style: _editTextStyle(),
+      ),
+      TextField(
+        obscureText: true,
+        maxLines: 1,
+        textInputAction: TextInputAction.next,
+        controller: _password,
+        focusNode: passwordFocusNode,
+        onSubmitted: (text) {
+          passwordFocusNode.unfocus();
+        },
+        decoration: InputDecoration(
+          labelText: app.password,
+        ),
+        style: _editTextStyle(),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          GestureDetector(
+            onTap: _getValidationCode,
+            child: Container(
+              width: 160.0,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: (bodyBytes != null)
+                    ? Image.memory(
+                        bodyBytes,
+                        fit: BoxFit.cover,
+                      )
+                    : Container(),
+              ),
+            ),
+          ),
+          SizedBox(width: 8.0),
+          Expanded(
+            child: TextField(
+              maxLines: 1,
+              textInputAction: TextInputAction.send,
+              controller: _validationCode,
+              focusNode: validationCodeFocusNode,
+              onSubmitted: (text) {
+                validationCodeFocusNode.unfocus();
+                _login();
+              },
+              decoration: InputDecoration(
+                labelText: '驗證碼',
+              ),
+              style: _editTextStyle(),
+            ),
+          ),
+        ],
+      ),
+      SizedBox(height: 8.0),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          GestureDetector(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Theme(
+                  data: ThemeData(
+                    unselectedWidgetColor: Colors.white,
+                  ),
+                  child: Checkbox(
+                    activeColor: Colors.white,
+                    checkColor: ApTheme.of(context).blue,
+                    value: isAutoLogin,
+                    onChanged: _onAutoLoginChanged,
+                  ),
+                ),
+                Text(app.autoLogin, style: TextStyle(color: Colors.white))
+              ],
+            ),
+            onTap: () => _onAutoLoginChanged(!isAutoLogin),
+          ),
+          GestureDetector(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Theme(
+                  data: ThemeData(
+                    unselectedWidgetColor: Colors.white,
+                  ),
+                  child: Checkbox(
+                    activeColor: Colors.white,
+                    checkColor: ApTheme.of(context).blue,
+                    value: isRememberPassword,
+                    onChanged: _onRememberPasswordChanged,
+                  ),
+                ),
+                Text(app.remember, style: TextStyle(color: Colors.white))
+              ],
+            ),
+            onTap: () => _onRememberPasswordChanged(!isRememberPassword),
+          ),
+        ],
+      ),
+      SizedBox(height: 8.0),
+      Container(
+        width: double.infinity,
+        child: RaisedButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(30.0),
+            ),
+          ),
+          padding: EdgeInsets.all(14.0),
+          onPressed: () {
+            //TODO FA
+            _login();
+          },
+          color: Colors.white,
+          child: Text(
+            app.login,
+            style: TextStyle(color: ApTheme.of(context).blue, fontSize: 18.0),
+          ),
+        ),
+      ),
+    ];
+    if (orientation == Orientation.portrait) {
+      list.addAll(listB);
+    } else {
+      list.add(Expanded(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center, children: listB)));
+    }
+    return list;
+  }
+
+  _onRememberPasswordChanged(bool value) async {
+    setState(() {
+      isRememberPassword = value;
+      if (!isRememberPassword) isAutoLogin = false;
+      Preferences.setBool(Constants.PREF_AUTO_LOGIN, isAutoLogin);
+      Preferences.setBool(Constants.PREF_REMEMBER_PASSWORD, isRememberPassword);
+    });
+  }
+
+  _onAutoLoginChanged(bool value) async {
+    setState(() {
+      isAutoLogin = value;
+      isRememberPassword = isAutoLogin;
+      Preferences.setBool(Constants.PREF_AUTO_LOGIN, isAutoLogin);
+      Preferences.setBool(Constants.PREF_REMEMBER_PASSWORD, isRememberPassword);
+    });
+  }
+
+  _getPreference() async {
+    isRememberPassword =
+        Preferences.getBool(Constants.PREF_REMEMBER_PASSWORD, true);
+    var username = Preferences.getString(Constants.PREF_USERNAME, '');
+    var password = '';
+    if (isRememberPassword) {
+      password =
+          Preferences.getStringSecurity(Constants.PREF_PASSWORD, '');
+    }
+    setState(() {
+      _username.text = username;
+      _password.text = password;
+    });
+  }
+
+  _login() async {
+    if (_username.text.isEmpty || _password.text.isEmpty) {
+      ApUtils.showToast(context, app.doNotEmpty);
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) => WillPopScope(
+          child: ProgressDialog(app.logining),
+          onWillPop: () async {
+            return false;
+          },
+        ),
+        barrierDismissible: false,
+      );
+      Preferences.setString(Constants.PREF_USERNAME, _username.text);
+      var response = await CourseHelper.instance.login(
+        username: _username.text,
+        password: _password.text,
+        validationCode: _validationCode.text,
+        callback: GeneralCallback(
+          onError: (GeneralResponse e) {
+            if (e.statusCode == 400) ApUtils.showToast(context, app.loginFail);
+          },
+          onFailure: (DioError e) {
+            ApUtils.showToast(context, ApLocalizations.dioError(context, e));
+          },
+        ),
+      );
+      Navigator.pop(context);
+      if (response != null) {
+        Preferences.setString(Constants.PREF_USERNAME, _username.text);
+        if (isRememberPassword) {
+          await Preferences.setStringSecurity(
+            Constants.PREF_PASSWORD,
+            _password.text,
+          );
+        }
+        Preferences.setBool(Constants.PREF_IS_OFFLINE_LOGIN, false);
+        ApUtils.showToast(context, app.loginSuccess);
+      } else
+        ApUtils.showToast(context, app.unknown);
+    }
+  }
+
+  void _getValidationCode() async {
+    bodyBytes = await CourseHelper.instance.getValidationImage();
+    setState(() {});
+    _validationCode.text = await OcrUtils.extractText(bodyBytes: bodyBytes);
+    setState(() {});
+  }
+}
