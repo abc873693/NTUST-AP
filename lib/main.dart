ae879b565d07c2ae0fed3b016c94d4ddb269ba4d
RainVisitor <abc873693@gmail.com>
2020-04-19 22:46:39 +0800

    Add firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:ap_common/utils/preferences.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 import 'app.dart';
 import 'config/constants.dart';
@@ -10,5 +15,18 @@ Future<void> main() async {
     key: Constants.key,
     iv: Constants.iv,
   );
-  runApp(MyApp());
+  if (!kIsWeb && (Platform.isIOS || Platform.isAndroid)) {
+    Crashlytics.instance.enableInDevMode = Constants.isInDebugMode;
+
+    // Pass all uncaught errors from the framework to Crashlytics.
+    FlutterError.onError = Crashlytics.instance.recordFlutterError;
+    runZoned<Future<void>>(() async {
+      runApp(MyApp());
+    }, onError: Crashlytics.instance.recordError);
+  } else {
+    // See https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+    runApp(MyApp());
+    //TODO add other platform Crashlytics
+  }
 }
