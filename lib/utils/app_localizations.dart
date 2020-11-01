0dca28740743943b144c85197a307870bf832c0d
RainVisitor <abc873693@gmail.com>
2020-11-02 00:22:20 +0800

    Update to v0.2.0

--- lib/utils/app_localizations.dart
+++ lib/utils/app_localizations.dart
@@ -29,7 +29,7 @@ class AppLocalizations {
     'en': {
       'app_name': 'NTUST AP',
       'update_note_content':
-          'Current feature list:\n* CourseTables\n* Course notify\n* Score History\n* Home page news\nAny problem can feedback to FB fans page~',
+          '* Fix all system.\nAny problem can feedback to FB fans page~',
       'about_open_source_content':
           'https://github.com/abc873693/NTUST-AP\n\nThis project is licensed under the terms of the MIT license:\nThe MIT License (MIT)\n\nCopyright &#169; 2020 Rainvisitor\n\nThis project is Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.',
       'needValidateCaptcha':
@@ -37,8 +37,7 @@ class AppLocalizations {
     },
     'zh': {
       'app_name': '台科校務通',
-      'update_note_content':
-          '目前可使用功能：\n* 課表查詢\n* 上課提醒\n* 學期成績查詢\n* 首頁最新消息\n有任何問題可反映粉絲專頁~',
+      'update_note_content': '* 修復所有系統\n有任何問題可反映粉絲專頁~',
       'about_open_source_content':
           'https://github.com/abc873693/NTUST-AP\n\n本專案採MIT 開放原始碼授權：\nThe MIT License (MIT)\n\nCopyright &#169; 2020 Rainvisitor\n\nThis project is Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.',
       'needValidateCaptcha': '需點擊機器人驗證\n完成後等待進入選課系統 並點擊右下角按鈕回到主畫面'
